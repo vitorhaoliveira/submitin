ffe0ef44615bb39e69af9d386ccad7d03bb6c38f
vitorhaoliveira <vitorhaoliveira123@gmail.com>
2025-12-30 22:57:16 -0300

    fix: build genarete schemas

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,8 +1,11 @@
+import path from "path";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
   transpilePackages: ["@submitin/ui", "@submitin/database", "@submitin/email", "@submitin/config"],
+
+  outputFileTracingRoot: path.join(__dirname, "../.."),
 };
 
 export default nextConfig;
-
